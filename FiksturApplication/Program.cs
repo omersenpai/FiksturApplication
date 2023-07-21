2cb43bf3b244859a3e0cd3cab06b962c78b6bb42
ÖmerSargin <96244199+omersenpai@users.noreply.github.com>
2023-07-21 16:05:11 +0300

    code duplication reduced

--- FiksturApplication/Program.cs
+++ FiksturApplication/Program.cs
@@ -1,5 +1,4 @@
 ﻿
-using FiksturApplication;
 using System;
 using System.Collections.Generic;
 
@@ -14,69 +13,33 @@ class Program
             "Konyaspor", "Antalyaspor", "Malatyaspor","Samsunspor",
             "Kayserispor", "Hatayspor", "Karagümrük", "Başakşehir",
             "Pendikspor","Adana DemirSspor", "Ç.Rizespor ","Ankaragücü"
-
-
         };
 
         //takımın fenerbahce olup olmadığı kontrolü
-        static bool IsFenerbahce(string team)
+        static bool Fikstur(string team)
         {
-            return team == "fenerbahçe" || team == "fenerbahce"|| team=="FENERBAHÇE" || team=="FENERBAHCE";
-
+            return team == "fenerbahçe" || team == "fenerbahce" ;
         }
 
+        Console.Write("Lütfen takım ismi giriniz: ");
+        var team = Console.ReadLine().ToLower();
 
-        //Fenerbahce Fikstür gösteren metod.Fixture class ını kullanıyoruz.
-        static void ShowFixtures(string team)
+        if (Fikstur(team))
         {
-            var fixtures = new List<Fixture>
-        {
-            new Fixture { Rakip = "Gaziantep FK",Konum = "Ev" },
-            new Fixture { Rakip = "Samsunspor", Konum = "Deplasman" },
-            new Fixture { Rakip = "Başakşehir", Konum = "Ev" },
-            new Fixture { Rakip = "Ankaragücü", Konum = "Deplasman" },
-            new Fixture { Rakip = "Antalya", Konum = "Ev" },
-            new Fixture { Rakip = "Alanya", Konum = "Deplasman" },
-            new Fixture { Rakip = "Ç. Rizespor", Konum = "Ev" },
-            new Fixture { Rakip = "Kasımpaşa", Konum = "Deplasman" },
-            new Fixture { Rakip = "Hatayspor", Konum = "Ev" },
-            new Fixture { Rakip = "Pendikspor",Konum="Deplasman"},
-            new Fixture { Rakip = "Trabzonspor",Konum="Ev"},
-            new Fixture { Rakip = "Adana DemirSpor",Konum="Deplasman"},
-            new Fixture { Rakip = "Karagümrük",Konum="Ev"},
-            new Fixture { Rakip = "SivasSpor",Konum="Ev"},
-            new Fixture { Rakip = "Beşiktaş",Konum="Deplasman"},
-            new Fixture { Rakip = "Konyaspor",Konum="Ev"},
-            new Fixture { Rakip = "Kayserispor",Konum="Deplasman"},
-            new Fixture { Rakip = "Galatasaray",Konum="Ev"},
-            new Fixture { Rakip = "İstanbulspor",Konum="Deplasman"},
-            new Fixture { Rakip = "Gaziantep FK",Konum="Deplasman"},
-            new Fixture { Rakip = "Samsunspor",Konum="Ev"},
-            new Fixture { Rakip ="Başakşehir",Konum="Deplasman"},
-        };
-
             Console.WriteLine($"\n{team.ToUpper()} FİKSTÜRÜ:");
             Console.WriteLine("---------------------------------------------------------------------------------");
             Console.WriteLine("|        RAKİP        |        KONUM        |");
             Console.WriteLine("----------------------------------------------------------------------------------");
 
-            foreach (var fixture in fixtures)
+            for (int i = 0; i < teams.Count; i++)
             {
-                Console.WriteLine($"| {fixture.Rakip,-19} | {fixture.Konum,-19} |");
-
-
+                if (teams[i].ToLower() != team.ToLower())
+                {
+                    var konum = i % 2 == 0 ? "Ev" : "Deplasman"; //çift sayıysa ev ,tek sayıysa deplasman 
+                    Console.WriteLine($"| {teams[i],-19} | {konum,-19} |"); 
+                }
             }
         }
-
-
-        Console.Write("Lütfen takım ismi giriniz: ");
-        var team = Console.ReadLine().ToLower();
-
-        if (IsFenerbahce(team))
-        {
-            ShowFixtures("Fenerbahçe");
-        }
-
         else if (!teams.Contains(team))
         {
             Console.WriteLine("Bu takım için bir fikstür mevcut değil.");
@@ -85,8 +48,5 @@ class Program
         {
             Console.WriteLine("Geçersiz takım adı.");
         }
-
-
-
     }
 }
